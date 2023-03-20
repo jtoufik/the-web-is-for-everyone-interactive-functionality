6f5822ab355f61423795278182c76a0733ebe873
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-03-20 14:59:36 +0100

    api werkend gekregen

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv'
+
+import express from 'express'
+import { fetchJson } from '../helpers/fetchWrapper.js'
+
+dotenv.config()
+
+const index = express.Router()
+
+// Overzicht
+index.get('/', (request, response) => {
+    let zoekopdracht = 'boek'
+    let booksUrl = `${process.env.API_URL}/search/?q=${zoekopdracht}&authorization=${process.env.API_KEY}&refine=true&output=json`
+
+    fetchJson(booksUrl).then((data) => {
+    response.render('index', data)
+        console.log(data)
+    })
+})
+
+export default index
---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import indexRoute from './routes/index.js'
+
+// Maak een nieuwe express app
+const server = express()
+
+// Stel het poortnummer in
+server.set('port', process.env.PORT || 8001)
+
+
+// Stel de view engine in
+server.set('view engine', 'ejs')
+server.set('views', './views')
+
+// Stel de public map in
+server.use(express.static('public'))
+
+// Stel afhandeling van formulieren in
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+
+// Stel de routes in
+server.use('/', indexRoute)
+
+// Start met luisteren
+server.listen(server.get('port'), () => {
+    console.log(`Application started on http://localhost:${server.get('port')}`)
+})
