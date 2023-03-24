778d0d29c23ad1a17e8957c91d600449c7058513
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-03-24 10:44:33 +0100

    .

--- routes/books.js
+++ routes/books.js
@@ -1,22 +1,22 @@
-import * as dotenv from 'dotenv'
+// import * as dotenv from 'dotenv'
 
-import { fetchJson, postJson } from '../helpers/fetchWrapper.js'
+// import { fetchJson, postJson } from '../helpers/fetchWrapper.js'
 
-import express from 'express'
+// import express from 'express'
 
-dotenv.config()
+// dotenv.config()
 
-const book = express.Router()
+// const book = express.Router()
 
-// Haal de gegevens van één boek op
-book.get('/', (request, response) => {
-    const id = request.query.id || null
-    const url = `${process.env.API_URL}/book/`
+// // Haal de gegevens van één boek op
+// book.get('/', (request, response) => {
+//     const id = request.query.id || null
+//     const url = `${process.env.API_URL}/book/`
 
-    fetchJson(url).then((data) => {
-        response.render('book', data)
-    })
-})
+//     fetchJson(url).then((data) => {
+//         response.render('book', data)
+//     })
+// })
 
 
-export default book
+// export default book
