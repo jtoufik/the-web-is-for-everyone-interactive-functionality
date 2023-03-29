a4481932e97a55cfc6a82c29f4e18767b973281a
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-03-29 12:08:25 +0200

    reserveer formulier gekoppeld

--- routes/books.js
+++ routes/books.js
@@ -18,7 +18,8 @@ const defaultUrl = urlBase + urlQuery + urlDefault + urlKey + urlOutput;
 
 // Maakt een route voor de detailpagina
 books.get("/", async (request, response) => {
-	let isbn = request.query.resultIsbn || "9789025764784";
+	let isbn = request.query.id || "9789025764784";
+	console.log(request.query)
 
 	const uniqueUrl = urlBase + urlQuery + isbn + urlKey + urlOutput;
 
@@ -30,5 +31,33 @@ books.get("/", async (request, response) => {
 	response.render("books", data);
 
 });
+
+// Toon het formulier om een reservering te maken
+books.get('/new', (request, response) => {
+	response.render('reserveForm.ejs')
+})
+
+// Handel het versturen van het formulier af
+books.post('/', (request, response) => {
+console.log(request.body)
+
+// // Roep de API aan met de post methode
+// const url = `${process.env.API_URL}/member`
+// postJson(url, request.body).then((data) => {
+// 	// De waarden uit het formulier (niet de API)
+// 	let newMember = { ...request.body }
+// 	// Het id uit de API (overschrijft het formulier)
+// 	newMember.id = data.data.createMember.id || null
+
+// 	// Stuur de gebruiker naar / als het gelukt is
+// 	if (data.success) {
+// 	response.redirect('/?memberPosted=true') // squad meegeven, message meegeven
+
+// 	// Toon opnieuw het formulier (met waarden) als het niet gelukt is
+// 	} else {
+// 	response.render('memberForm.ejs', newMember) // Fail, message meegeven
+// 	}
+// })
+})
 
 export default books
